e65aa3ed6b7ff53416b1c39a5a06e4b3cbefa25c
Eduardo Hoffmann <eduardohoffmanndocarmosilva@gmail.com>
2024-10-14 20:20:07 -0300

    primeiro commit

---
+++ exercicioAula2/main.js
@@ -0,0 +1,15 @@
+//6-Crie uma função que receba seu nome como parâmetro e retorne uma saudação.
+
+function saudacao(nome){
+    console.log("Olá, " + nome + "!")
+}
+
+saudacao("Eduardo")
+
+//7-Crie uma função que receba dois números como parâmetro e retorne a soma deles.
+
+function soma(num1, num2){
+    return num1 + num2
+}
+
+console.log("Soma: " + soma(2, 3))
